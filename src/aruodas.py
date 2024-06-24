de8a91b8441301120b55c4a6e7dfeaca278a839e
audrbar <audrbar@gmail.com>
2024-06-24 19:39:25 +0300

    Upd: etar and aruodas prepare df

--- src/aruodas.py
+++ src/aruodas.py
@@ -5,137 +5,114 @@ import re
 import pandas as pd
 import seaborn as sns
 from lxml import html
-import requests
 import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-# ----------------------------Scrape Data--------------------
+print('\n-------------------1. Scrape Data----------------------')
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.aruodas.lt/sklypai-pardavimui/")
 time.sleep(5)
 page = driver.page_source
-# -------------------------Read Data from File----------------
+print('- Done!')
+print('\n----------2. Write to File and Read from it-----------')
 # driver = webdriver.Chrome()
-# file_path = os.path.abspath('aruodas.html')
+# file_path = os.path.abspath('aruodas_raw.html')
 # file_url = f'file://{file_path}'
 # print(file_url)
 # driver.get(file_url)
-# ---------------------Read Data from Scraped HTML-----------
+print('- Not this time.')
+print('\n--------------3. Read Data from Scraped HTML-------------')
 locations = driver.find_elements(By.CLASS_NAME, 'list-adress-v2')
-# print(addresses)
+print('- Locations Obj (len:', len(locations), ')', locations)
+area_overall = driver.find_elements(By.CLASS_NAME, 'list-AreaOverall-v2')
+print('- Area Overall Obj (len:', len(area_overall), ')', area_overall)
+intendance = driver.find_elements(By.CLASS_NAME, 'list-Intendances-v2')
+print('- Intendance Obj (len:', len(intendance), ')', intendance)
+print('\n------------------4. Make Data Lists------------------')
 locations_list = []
-for location in locations:
-    locations_list.append(location.text)
-del locations_list[:2]
-temp = []
-town_list = []
-town_list_d = []
-
-for item in locations_list:
-    temp = item.split("\n")
-    town_list_d.append(temp[0])
-
-for item in town_list_d:
-    temp = item.split(',')
-    town_list.append(temp[0])
-# print('Location: ', locations_list)
-print('Town: ', town_list)
-
-prices_d = driver.find_elements(By.CLASS_NAME, 'list-item-price-v2')
-price_list = []
-for item in prices_d:
-    stripped = item.text.strip()
-    stripped_s = stripped[:-2]
-    price_list.append(stripped_s)
-print('Prices: ', price_list)
-
-prices_pm_d = driver.find_elements(By.CLASS_NAME, 'price-pm-v2')
-price_pm_list = []
-for item in prices_pm_d:
-    stripped = item.text.strip()
-    split = stripped.split('€')
-    price_pm_list.append(split[0])
-print('Prices_pm: ', price_pm_list)
-
-area_d = driver.find_elements(By.CLASS_NAME, 'list-detail-v2')
-area_list_d = []
-for item in area_d:
-    area_list_d.append(item.text)
-area_list = area_list_d[0::2]
-type_list = area_list_d[1::2]
-print('Area: ', area_list)
-print('Type: ', type_list)
-# -------------Create Data Frame and write it to File-------------
+for item in locations:
+    locations_list.append(item.text.strip())
+print('- Locations List (len:', len(locations_list), ')', locations_list)
+area_overall_list = []
+for item in area_overall:
+    area_overall_list.append(item.text.strip())
+print('- Area Overall List (len:', len(area_overall_list), ')', area_overall_list)
+intendance_list = []
+for item in intendance:
+    intendance_list.append(item.text.strip())
+print('- Intendance List (len:', len(intendance_list), ')', intendance_list)
+print('\n-------------5. Create Awesome Data Frame-----------------')
 df = pd.DataFrame({
-    'Prices': price_list,
-    'Prices_pm': price_pm_list,
-    'Area': area_list,
-    'Type': type_list,
-    'Town': town_list
+    'Location': locations_list,
+    'Intendance':intendance_list,
+    'Area_Overall':area_overall_list
 })
-df.to_csv("aruodas_first.csv", index=False)
-print(df)
-# -----------Read Data Frame from File and Explore it-------------
-df_d = pd.read_csv("aruodas_first.csv")
-# print(df_d)
-df = df_d.drop(df_d.columns[[0]], axis=1)
-print('----Shape----\n', df.shape)
-print('----Head----\n', df.head(5))
-print('----Columns----\n', df.columns)
-print('----Dtypes----\n', df.dtypes)
-print('----Is None?----\n', df.isna().sum())
-print('----Is Duplicated?----\n', df.loc[df.duplicated()])
-# ------------Data Preparation-----------------------------------
-df['Prices'] = df['Prices'].str.strip()
-df['Prices_pm'] = df['Prices_pm'].str.strip()
-df["Prices"] = df['Prices'].str.replace(r'\s+', '', regex=True)
-df["Prices_pm"] = df['Prices_pm'].str.replace(r'\s+', '', regex=True)
-df['Prices'] = df['Prices'].astype(int)
-df['Prices_pm'] = df['Prices_pm'].astype(int)
-# df['Prices_pm'] = df['Prices_pm']str.replace.astype(int)
-
-print('----Dtypes refactored----\n', df.dtypes)
-print('Data Frame Prepared:\n', df)
-print('------------And magic begins: Charts----------------\n')
-# ---------------------Draw Pie---------------------------------
-town_counts = df["Town"].value_counts()
-t_labels = list(town_counts.index)
+print('\n- Data Frame:\n', df.head(5))
+print('\n---------6. Write Data Frame to file and Read it-----------')
+# df.to_csv("aruodas_df.csv", index=False)
+# df = pd.read_csv("aruodas_df.csv")
+print('- Not this time.')
+print('\n---------7. Prepare Data Frame for analysis--------------')
+df = df.drop(df.head(2).index) # drop first n rows
+df['Discount'] = df['Location'].str.split(pat='\n').str[2].str.strip()
+df['Price (€)'] = df['Location'].str.split(pat='\n').str[-2].str.strip()
+df['Price (€/a)'] = df['Location'].str.split(pat='\n').str[-1].str.strip()
+df['Location'] = df['Location'].str.split(pat='\n').str[0].str.split(pat=',').str[0].str.strip()
+df['Area_Overall'] = df['Area_Overall'].astype(float)
+df['Price (€)'] = df['Price (€)'].str.split(pat='€').str[0].str.replace(r'\s+', '', regex=True).astype(int)
+df['Price (€/a)'] = df['Price (€/a)'].str.split(pat='€').str[0].str.replace(r'\s+', '', regex=True).astype(int)
+df['Discount'] = df['Discount'].str.replace(r'(^.*€.*$)', '0%', regex=True).str.replace(r'%', '', regex=True).str.split(' ').str[-1].str.replace(',', '.').astype(float)
+print('\n----------------8. Explore Data Frame--------------------')
+print('\n- Shape: ', df.shape)
+print('\n- Head:\n', df.head())
+print('\n- Columns:\n', df.columns)
+print('\n- Dtypes:\n', df.dtypes)
+print('\n- Is None?\n', df.isna().sum())
+print('\n- Is Duplicated?\n', df.loc[df.duplicated()])
+print('\n- Is Null?\n', df.isnull().any())
+print('\n------------9. And magic begins: Draw Charts-------------')
+# ------------------Draw Pie-----------------------------------
+area_count = df["Location"].value_counts()
+t_labels = list(area_count.index)
 plt.figure(figsize=(8, 6))
-plt.pie(town_counts, labels=t_labels, autopct='%1.1f%%', startangle=130)
-plt.title("Pasiulymu kiekis pagal vietove")
+plt.pie(area_count, labels=t_labels, autopct='%1.1f%%', startangle=130)
+plt.title("Pasiulymu kiekis pagal vietove, vnt.")
 plt.show()
 # ------------------Draw Bar-----------------------------------
 plt.figure(figsize=(10, 6))
-plt.barh(df["Type"], df["Prices"], color='orange')
-plt.title("Zemes paskirtis vs objekto kaina")
-plt.xlabel("Objekto kaina (Eur)")
+plt.barh(df["Intendance"], df["Price (€/a)"], color='orange')
+plt.title("Zemes paskirtis vs aro kaina")
+plt.xlabel("Kaina (€/a)")
 plt.ylabel("Zemes paskirtis")
 plt.show()
 # --------------------Draw Histogram----------------------------
-price_pm_counts = df["Prices_pm"]
+price_pm_counts = df["Price (€/a)"]
 plt.figure(figsize=(6, 6))
 plt.hist(price_pm_counts)
-plt.title("Pasiulymu kiekis pagal vieneto kaina")
-plt.xlabel("Kaina uz ara (Eur)")
-plt.ylabel("Kiekis (vnt.)")
+plt.title("Pasiulymai vs. aro kaina")
+plt.xlabel("Kaina (€/a)")
+plt.ylabel("Pasiulymai (vnt.)")
 plt.show()
 # -------------------Draw Scatter-----------------------------
 plt.figure(figsize=(10, 6))
-plt.scatter(df['Prices'], df['Type'], c='blue')
-plt.title("Vieneto kaina vs zemes paskirtis")
-plt.xlabel("Objekto kaina (Eur)")
+plt.scatter(df['Price (€/a)'], df['Intendance'], c='blue')
+plt.title("Aro kaina vs zemes paskirtis")
+plt.xlabel("Kaina (€/a)")
 plt.ylabel("Zemes paskirtis")
 plt.show()
 # ------------------Draw Scatter Seaborn---------------------
 plt.figure(figsize=(10, 6))
-ax = sns.scatterplot(x='Prices_pm', y='Type', hue='Town', data=df)
-ax.set_title('Vieneto kaina vs zemes paskirtis vs vietove')
+ax = sns.scatterplot(x='Price (€/a)', y='Intendance', hue='Location', data=df)
+ax.set_title('Aro kaina vs zemes paskirtis vs vietove')
 plt.show()
 # ------------------Draw Pair plot Seaborn-------------------
 sns.pairplot(df)
+plt.show()
+# ------------------Draw Pair plot Seaborn HUE-------------------
+sns.pairplot(df, hue="Location")
 plt.show()
--- src/etar.py
+++ src/etar.py
@@ -21,28 +21,32 @@ tree = html.fromstring(page.content)
 print('----HTML Tree----\n', tree)
 # ----------Read Data from Scraped HTML and create Data Table-------
 td_title = tree.xpath('//div[@class="ui-datatable-tablewrapper"]//table//td/a/text()')
-del td_title[-1]
 print('----Title----\n', len(td_title), td_title)
-td_date_entry = tree.xpath('//div[@class="ui-datatable-tablewrapper"]//table//td/span/text()')
+td_date_entry_d = tree.xpath('//div[@class="ui-datatable-tablewrapper"]//table//td/span/text()')
+td_date_entry = td_date_entry_d + ['2024-06-22']
 print('Date_entry: ', len(td_date_entry), td_date_entry)
 rows = tree.xpath('//div[@class="ui-datatable-tablewrapper"]//table//tr')
-print('----Table Rows----\n', rows)
+print('----Table Rows----\n', len(rows), rows)
 table_data = []
 for row in rows:
     cells = row.xpath('.//td/text()')
-    table_data.append([cell.strip() for cell in cells])
-print('----Table Data for DF-----\n: ', table_data)
-# -------------------Create Data Frame and prepare data---------------------------
-df_d = pd.DataFrame(table_data)
-df = df_d.drop(df_d.columns[[7, 8]], axis=1)
-df.drop(df.head(1).index, inplace=True) # drop first n rows
-df.drop(df.tail(4).index,inplace=True) # drop last n rows
-df.columns = ['ID', 'Legal_act_type', 'Author', 'Legal_act_id', 'Legal_act_number', 'Legal_act_date', 'Registry_date']
+    if cells != []:
+        table_data.append([cell.strip() for cell in cells])
+print('----Table Data for DF-----\n', len(table_data))
+for i in table_data:
+    print(i)
+# -------------------Create Data Frame and Prepare data---------------------------
+df = pd.DataFrame(table_data)
+df = df.drop(df.columns[[0, 7, 8]], axis=1)
+# df.drop(df.head(1).index, inplace=True) # drop first n rows
+# df.drop(df.tail(4).index,inplace=True) # drop last n rows
+df.columns = ['Legal_act_type','Author','Legal_act_id','Legal_act_number','Legal_act_date','Registry_date']
 df['Date_entry'] = td_date_entry
 df['Title'] = td_title
-df['Author'] = df['Author'].str.replace('Priėmė\n                                    \n                                        ', '')
-df['Legal_act_id'] = df['Legal_act_id'].str.replace('Identifikacinis kodas ', '')
-df['Legal_act_date'] = pd.to_datetime(df['Legal_act_date'])
+df = df.rename(columns={'Legal_act_type':'Act_type', 'Author':'Act_issuer', 'Legal_act_id':'Act_identifier', 'Legal_act_number':'Act_number', 'Legal_act_date':'Act_date'})
+df['Act_issuer'] = df['Act_issuer'].str.split(pat='\n').str[2].str.strip()
+df['Act_identifier'] = df['Act_identifier'].str.replace('Identifikacinis kodas ', '')
+df['Act_date'] = pd.to_datetime(df['Act_date'])
 df['Registry_date'] = pd.to_datetime(df['Registry_date'])
 df['Date_entry'] = pd.to_datetime(df['Date_entry'])
 # ---------------Write DF to File and Read from it----------------
@@ -50,23 +54,30 @@ df['Date_entry'] = pd.to_datetime(df['Date_entry'])
 # df = pd.read_csv("etar.csv")
 # print(df)
 # --------------------Explore Data Frame--------------------------
+df['Act_number']
 print('----Shape----\n', df.shape)
 print('----Head----\n', df.head())
 print('----Columns----\n', df.columns)
 print('----Dtypes----\n', df.dtypes)
+print('----Is Null?----\n', df.isnull().any())
+# df[df['Act_type'].isna()==1]
+# df['Act_type'] = df['Act_type'].fillna(0)
+# df[df['Act_type'].duplicated()==1]
+# df[df['Act_type'].isin(['Del kazko', 'Del kitko'])]
+# df.drop_duplicates()
 print('----Is None?----\n', df.isna().sum())
 print('----Is Duplicated?----\n', df.loc[df.duplicated()])
 # ---------------------Draw Pie---------------------------------
-auth_counts = df["Author"].value_counts()
+auth_counts = df["Act_issuer"].value_counts()
 t_labels = list(auth_counts.index)
 plt.figure(figsize=(8, 6))
 plt.pie(auth_counts, labels=t_labels, autopct='%1.1f%%', startangle=130)
 plt.title("Teises aktai pagal sudarytoja")
 plt.show()
 
-type_counts = df["Legal_act_type"].value_counts()
+type_counts = df["Act_type"].value_counts()
 type_labels = list(type_counts.index)
-plt.figure(figsize=(6, 6))
+plt.figure(figsize=(8, 6))
 plt.pie(type_counts, labels=type_labels, autopct='%1.1f%%', startangle=130)
 plt.title("Legal Act Types")
 plt.show()
